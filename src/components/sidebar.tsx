6bb0faa26759ac77e23a1326a0234e1da9c80af3
aka <aaronkaltman@gmail.com>
2024-01-17 04:59:52 -0500

    not quite getting the prisma

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,16 +1,9 @@
-import React from "react";
-
 export default function Sidebar() {
   return (
     <div className="flex flex-col h-94 w-full md:w-1/3 bg-stone-200 rounded-sm">
       <h2 className="px-4 py-4">Filtering and results go here</h2>
       <ul className="px-4">
-        <li>job 1</li>
-        <li>job 1</li>
-        <li>job 1</li>
-        <li>job 1</li>
-        <li>job 1</li>
-        <li>job 1</li>
+        <li>hi</li>
       </ul>
     </div>
   );
